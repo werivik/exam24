82e6952d3dfc04a028809f5b3b95d32b26e554db
werivik <wervik00994@stud.noroff.no>
2024-04-19 17:59:40 +0200

    trying the access token stuff

--- JS/auth-login.js
+++ JS/auth-login.js
@@ -1,7 +1,51 @@
-const options = {  
-    headers: {    
-      Authorization: `Bearer ${accessToken}`,    
-      "X-Noroff-API-Key": apiKey.data.key  
-    }} 
-    const response = await fetch(`${NOROFF_API_URL}/social/posts`, options)
-    const data = await response.json()
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Trying to sneak a peak?...");
+
+    const loginForm = document.querySelector("form#login");
+
+    loginForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        console.log("Trying to Login, dont interrupt");
+
+        const username = loginForm.username.value.trim();
+        const password = loginForm.password.value.trim();
+
+        console.log("username: ", username);
+        console.log("password: ", password);
+
+        if (username && password) {
+            console.log("Heading out to get Token, One sec")
+            getToken(username, password);
+        }
+    });
+
+    async function getToken(username, password) {
+        try {
+            const loginData = {
+                email: username,
+                password: password
+            };
+
+            const options = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(loginData),
+            };
+
+            const response = await fetch(`https://v2.api.noroff.dev/auth/login`, options);
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+                localStorage.setItem("username", data.data.name);
+                localStorage.setItem("token", data.data.accessToken);
+                window.location = "/HTML/admin-page.html";
+            } else {
+                throw new Error(response.statusText);
+            }
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+});
