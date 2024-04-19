137757b800de45740d84d8cf0efd3f008ef83b72
werivik <wervik00994@stud.noroff.no>
2024-04-19 18:54:17 +0200

    is it gonna work?

---
+++ JS/admin-create.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const newBlogForm = document.getElementById("newBlogForm");
+
+    newBlogForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const title = document.getElementById("title").value.trim();
+        const body = document.getElementById("body").value.trim();
+        const tags = document.getElementById("tags").value.trim().split(",");
+        const mediaUrl = document.getElementById("mediaUrl").value.trim();
+        const mediaAlt = document.getElementById("mediaAlt").value.trim();
+
+        try {
+            const postData = {
+                title: title,
+                body: body,
+                tags: tags,
+                media: {
+                    url: mediaUrl,
+                    alt: mediaAlt
+                }
+            };
+
+            const options = {
+                method: "POST", 
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(postData)
+            };
+
+            const response = await fetch("https://v2.api.noroff.dev/blog/posts/wervik", options);
+
+            if (response.ok) {
+                alert("Blog Post Created Successfully <3")
+                window.location.href = "/HTML/admin-all.html";
+            }
+
+            else {
+                throw new Error("Failed to Create New Post, try again");
+            }
+        }
+
+        catch (error) {
+            console.error(error.message);
+            alert("Failed to Create Blog Post, Please never try again");
+        }
+    });
+});
