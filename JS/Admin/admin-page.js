b621e56702c7127e7e11b80492b88264f9a913bb
werivik <wervik00994@stud.noroff.no>
2024-05-08 18:08:25 +0200

    tweaking the tweaks

--- JS/Admin/admin-page.js
+++ JS/Admin/admin-page.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const allBlogsContainer = document.querySelector(".all-blogs");
+    
     if (!allBlogsContainer) {
         console.error("Element with class 'all-blogs' not found.");
+
         return;
     }
 
@@ -12,14 +14,19 @@ document.addEventListener("DOMContentLoaded", () => {
                 throw new Error("Failed to Fetch Data from API, try again, loser");
             }
             const data = await response.json();
+
             return data;
-        } catch (error) {
+
+        } 
+        
+        catch (error) {
             console.error("Error fetching data:", error);
         }
     };
 
     const updateInfo = async () => {
         const data = await fetchBlogData();
+
         if (data && data.data && Array.isArray(data.data)) {
             const totalPosts = data.data.length;
             const lastPosted = data.data[0].created;
@@ -27,22 +34,26 @@ document.addEventListener("DOMContentLoaded", () => {
             const totalPostsElement = document.getElementById("totalPosts");
             const lastPostedElement = document.getElementById("lastPosted");
 
-            if(totalPostsElement && lastPostedElement) {
+            if (totalPostsElement && lastPostedElement) {
                 totalPostsElement.textContent = totalPosts;
                 lastPostedElement.textContent = formatDate(lastPosted);
-            } else {
+            } 
+            
+            else {
                 console.error("Elements with IDs 'totalPosts' and 'lastPosted' not found.");
             }
         }
     };
 
     const formatDate = (dateString) => {
+
         const date = new Date(dateString);
         const day = date.getDate();
         const month = date.getMonth() + 1;
         const year = date.getFullYear();
         const formattedDay = day < 10 ? `0${day}` : day;
         const formattedMonth = month < 10 ? `0${month}` : month;
+
         return `${formattedDay}.${formattedMonth}.${year}`;
     };
 
@@ -51,7 +62,9 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch("https://v2.api.noroff.dev/blog/posts/wervik")
         .then((response) => response.json())
         .then((data) => {
+
             if (data && Array.isArray(data.data)) {
+
                 data.data.forEach((blog) => {
                     const blogElement = document.createElement("div");
                     blogElement.classList.add("blog-post");
@@ -64,16 +77,21 @@ document.addEventListener("DOMContentLoaded", () => {
                             </div>
                         </a>
                     `;
-                    if(allBlogsContainer) {
+                    if (allBlogsContainer) {
                         allBlogsContainer.appendChild(blogElement);
-                    } else {
+                    } 
+                    
+                    else {
                         console.error("Element with class 'all-blogs' not found.");
                     }
                 });
-            } else {
+            } 
+            
+            else {
                 console.error("Invalid data Format received from API");
             }
         })
+
         .catch((error) => {
             console.error("Failed to fetch Blog Posts", error);
         });
